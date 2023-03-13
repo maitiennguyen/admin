cdb6c0f9dc8a21a4bb5a387f6893b798f8f786a0
maitiennguyen <117673140+maitiennguyen@users.noreply.github.com>
2023-03-12 18:53:32 -0600

    user can enter "cancel" to leave enter report ID prompt and return home

--- AccessReport.java
+++ AccessReport.java
@@ -6,6 +6,9 @@ public class AccessReport extends FileReport{
     public void showReport() {
         System.out.println("Access Report (Enter 'cancel' to return home)\n");
         reportID = this.reportIDPrompt();
+        if (reportID.equals("cancel")) {
+            return;
+        }
         this.report = save.retrieveReport(reportID); // retrieve report
         this.editReport(); // display and edit report
         save.updateReport(this.report); // update edit if any
